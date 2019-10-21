134987312ed7fad29859d9308fb8956ade55a795
Jim Barry <jbarry@pelicom.net>
2019-10-21 12:21:06 -0500

    ask ? about unknown class error

--- MemeMe2_0/MemeCollectionViewCell.swift
+++ MemeMe2_0/MemeCollectionViewCell.swift
@@ -2,16 +2,13 @@
 //  MemeCollectionViewCell.swift
 //  MemeMe2_0
 //
-//  Created by James Barry on 10/15/19.
+//  Created by James Barry on 10/19/19.
 //  Copyright © 2019 Jim Barry. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 class MemeCollectionViewCell: UICollectionViewCell {
-    
-    // MARK: Outlets
     
     @IBOutlet weak var memeImageView: UIImageView!
     
---
+++ MemeMe2_0/MemeDetailViewControllerTestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemeDetailViewControllerTestViewController.swift
+//  MemeMe2_0
+//
+//  Created by James Barry on 10/19/19.
+//  Copyright © 2019 Jim Barry. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewControllerTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
